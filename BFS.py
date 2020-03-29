96af669c09864e132b08f0b83f6de0127f15089b
berk.sonmezer <berk.sonmezer@ozu.edu.tr>
2020-03-29 17:04:02 +0300

    implement bfs

--- BFS.py
+++ BFS.py
@@ -9,9 +9,24 @@ class BFS:
 
     def run(self, target):
         """ YOUR CODE HERE """
+        match_found = False
+        counter = 0
+        depth = 0
+        while len(self.queue) != 0:
+            counter = counter + 1
+            candidate_node = self.queue.pop()
+            if candidate_node.is_equal(target):
+                match_found = True
+                depth = candidate_node.step
+                break
+
+            children_nodes_list = self.graph.reveal_neighbors(candidate_node)
+            for child_node in children_nodes_list:
+                self.queue.insert(0, child_node)
 
         # return 3 items
         # a: bool (match found or not)
         # b: int (counter)
         # c: int (depth)
-        return False, 0, 0
+        # return False, 0, 0
+        return match_found, counter, depth
